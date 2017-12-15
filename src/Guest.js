d6c622cc4bf8a4f0533fa0356d3fa42bc747b942
The Tien <tiendt2311@gmail.com>
2017-12-15 17:09:16 +0700

    init app

---
+++ src/Guest.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Guest = props => 
+  <li>
+    <span>{props.name}</span>
+    <label>
+      <input type="checkbox" checked={props.isConfirmed} 
+        onChange={props.handleConfirmation.bind(this)}/> 
+    </label>
+    <button onClick={props.handleEditing.bind(this)}>edit</button>
+    <button>remove</button>
+  </li>;
+
+Guest.propTypes = {
+  name: PropTypes.string.isRequired,
+  isConfirmed: PropTypes.bool.isRequired,
+  isEditing: PropTypes.bool.isRequired,
+  handleConfirmation: PropTypes.func.isRequired,
+  handleEditing: PropTypes.func.isRequired
+}
+
+export default Guest;
